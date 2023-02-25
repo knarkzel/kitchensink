4f8abb5ed9ba8442b686f4de604fa0b155adb5b4
Knarkzel <knarkzel@gmail.com>
2023-02-25 17:24:20 +0100

    Added helpers

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,61 @@
 // Modules
+pub mod discord;
 pub mod home;
 pub mod login;
-pub mod signup;
 pub mod navbar;
-pub mod discord;
+pub mod signup;
+
+use std::collections::HashMap;
 
 // Imports
 pub use dioxus::prelude::*;
+
+// Constants
+const SUPABASE_ENDPOINT: &str = "https://ojjkwixnlbiakfnhaksp.supabase.co";
+const SUPABASE_ANON_KEY: &str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9qamt3aXhubGJpYWtmbmhha3NwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzczMzMyODksImV4cCI6MTk5MjkwOTI4OX0.nVHH5sgXztN-3eweYHOD5eGi6g7Msj2U4RxJ0pCtHiU";
+
+// Supabase
+pub struct Supabase;
+
+impl Supabase {
+    pub fn new() -> postgrest::Postgrest {
+        postgrest::Postgrest::new(format!("{SUPABASE_ENDPOINT}/rest/v1"))
+            .insert_header("apikey", SUPABASE_ANON_KEY)
+    }
+}
+
+// Regular HTTP client
+use reqwest::Response;
+
+pub struct Client {
+    client: reqwest::Client,
+}
+
+impl Client {
+    pub fn new() -> Self {
+        Self {
+            client: reqwest::Client::new(),
+        }
+    }
+
+    pub async fn signup(&self, email: &str, password: &str) -> Result<Response, reqwest::Error> {
+        let body = HashMap::from([("email", email), ("password", password)]);
+        self.client
+            .post(format!("{SUPABASE_ENDPOINT}/auth/v1/signup"))
+            .header("apikey", SUPABASE_ANON_KEY)
+            .json(&body)
+            .send()
+            .await
+    }
+
+    pub async fn login(&self, email: &str, password: &str) -> Result<Response, reqwest::Error> {
+        let body = HashMap::from([("email", email), ("password", password)]);
+        self.client
+            .post(format!("{SUPABASE_ENDPOINT}/auth/v1/token"))
+            .header("apikey", SUPABASE_ANON_KEY)
+            .query(&[("grant_type", "password")])
+            .json(&body)
+            .send()
+            .await
+    }
+}
